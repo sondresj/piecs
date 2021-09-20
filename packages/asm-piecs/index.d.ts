8a37a6d362ba74badf354d2d7abaffebfe94c1c6
Sondre <sondre.skaardal.jensen@gmail.com>
2021-09-20 19:45:17 +0200

    New repo structure, one for js implementation and one for asm

---
+++ packages/asm-piecs/index.d.ts
@@ -0,0 +1,16 @@
+type i8 = number;
+type i16 = number;
+type i32 = number;
+type i64 = bigint;
+type isize = number;
+type u8 = number;
+type u16 = number;
+type u32 = number;
+type u64 = bigint;
+type usize = number;
+type f32 = number;
+type f64 = number;
+type bool = boolean | number;
+export function add(a: i32, b: i32): i32;
+export const memory: WebAssembly.Memory;
+export const __setArgumentsLength: ((n: i32) => void) | undefined;
---
+++ packages/piecs/src/__tests__/asm.test.ts
@@ -0,0 +1,7 @@
+import { add } from 'asm-piecs'
+
+describe('asm-piecs', () => {
+    it('works', () => {
+        expect(add(1, 2)).toBe(3)
+    })
+})
