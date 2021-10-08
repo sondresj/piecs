7a3bb7c832ebf100c29212c23e1f5a7eda25065b
Sondre <sondre.skaardal.jensen@gmail.com>
2021-10-08 22:47:20 +0200

    update index exports

--- packages/piecs/src/index.ts
+++ packages/piecs/src/index.ts
@@ -1,7 +1,9 @@
 import { World } from './World'
 
-export { and, or, not, any, all, prefab, query } from './Query'
 export type { Query } from './Query'
+export { and, or, not, any, all, prefab, query } from './Query'
+export type { WorldStatistics } from './utils'
+export { getStatistics } from './utils'
 export type { Archetype } from './Archetype'
 export type { System } from './types'
 export { World }
